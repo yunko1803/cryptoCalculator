3639b308828c48f5bd3b47f70877414e17c1169a
Yoon Koh <yk1803@nyu.edu>
2019-09-12 13:51:20 -0400

    second commit

--- app/javascript/components/Calculate.js
+++ app/javascript/components/Calculate.js
@@ -8,11 +8,11 @@ class Calculate extends Component {
   render() {
     return (
       <div>
-        <h1>How much {this.props.active_currency} do you own?</h1>
+        <h1>How much {this.props.active_currency.name} do you own?</h1>
         <form onSubmit={this.props.handleSubmit}>
           <div className="form-group">
             <label>Enter amount owned: </label> <br />
-            <input onChange={this.props.handleChange} className="field" autoComplete="off" type="text" name="name" placeholder="How much do you onw?" value={this.props.amount} />
+            <input onChange={this.props.handleChange} className="field" autoComplete="off" type="text" name="amount" placeholder="How much do you onw?" value={this.props.amount} />
           </div>
           <div className="form-group" >
             <input type="submit" className="calculate-btn" value="Calculate My Total"></input>
--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Search from './Search';
 import Calculate from './Calculate';
+import Portfolio from './Portfolio';
 import axios from 'axios';
 
 class PortfolioContainer extends Component {
@@ -14,6 +15,8 @@ class PortfolioContainer extends Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
+    this.handleAmount = this.handleAmount.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSelect(e) {
@@ -23,7 +26,6 @@ class PortfolioContainer extends Component {
     this.setState({
       active_currency: activeCurrency[0],
     })
-    debugger
   }
 
   handleChange(e) {
@@ -31,7 +33,7 @@ class PortfolioContainer extends Component {
     // this.setState({
     //   [e.target.name]: e.target.value
     // })
-    console.log(e.target.value);
+    // console.log(e.target.value);
     axios.post('http://localhost:3000/search', {
       search: e.target.value
     })
@@ -45,24 +47,59 @@ class PortfolioContainer extends Component {
     })
     console.log(this.state.search_results);
   }
+
   handleAmount(e) {
+    console.log("hey");
     this.setState({
       [e.target.name]:e.target.value
     })
   }
-  handleSumbit(e) {
 
+  handleSubmit(e) {
+    e.preventDefault();
+    let currency = this.state.active_currency;
+    let amount = this.state.amount;
+
+    axios.post('http://localhost:3000/calculate', {
+      id: currency.id,
+      amount:amount
+    })
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          amount:"",
+          active_currency:null,
+          portfolio: [...this.state.portfolio, data.data],
+          search_results:[],
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  
   }
 
   render() {
-
+    // this.handleAmount()
     const searchOrCalculate = this.state.active_currency ?
-      <Calculate hanleChange={this.handleAmount} handleSubmit={this.handleSubmit} active_currency={this.state.active_currency} amount={this.state.amount}/> :
-      <Search searchResults={this.state.search_results} handleSelect={this.handleSelect} name={this.state.name} handleChange={this.handleChange} />
+      <Calculate handleChange={this.handleAmount}
+        handleSubmit={this.handleSubmit}
+        active_currency={this.state.active_currency}
+        amount={this.state.amount} /> :
+      <Search searchResults={this.state.search_results}
+        handleSelect={this.handleSelect}
+        name={this.state.name}
+        handleChange={this.handleChange} />
     
     return (
-      <div>
-        {searchOrCalculate}
+      <div className="grid">
+        <div className="left">
+          {searchOrCalculate}
+        </div>
+        <div className="right">
+          <Portfolio portfolio={this.state.portfolio} />
+        </div>
+        
       </div>
       
     )
